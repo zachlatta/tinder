c0fe1d99fa21dc2ca9e53c8b186b4ed8ecf1dec1
Norwack <linuxguy@1337.no>
2015-02-03 15:59:25 +0100

    removed structs

--- tinder.go
+++ tinder.go
@@ -1,28 +1,22 @@
 package tinder
 
 import (
+	"net/http"
 	"net/url"
 )
-
-type Tinder struct {
-	Host      string
-	Facebook  map[string]string
-	Headers   url.Values
-}
 
 func Init(FacebookUserId string, FacebookToken string) *Tinder {
 	Host := "https://api.gotinder.com"
 	Facebook := make(map[string]string)
 	Facebook["facebook_token"] = FacebookToken
 	Facebook["facebook_id"] = FacebookUserId
-	Values := &url.Values{
-		"Content-Type": {"application/json"},
-		"User-Agent": {"Tinder/3.0.4 (iPhone; iOS 7.1; Scale/2.00)"},
-	}
+	Values := &url.Values{}
+	Client := &http.Client{}
 
 	return &Tinder{
 		Host: Host,
 		Facebook: Facebook,
 		Headers: *Values,
+		Client: *Client,
 	}
 }
