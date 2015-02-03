91bf01987f6442b7a5f8329db485402aa665c6e9
Norwack <linuxguy@1337.no>
2015-02-03 16:00:01 +0100

    major changes and added function to edit/update profile

--- requests.go
+++ requests.go
@@ -4,11 +4,8 @@ import (
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
+	"bytes"
 )
-
-type User struct {
-	Token	string
-}
 
 func (tinder *Tinder) Auth() error {
 	Headers := tinder.Headers
@@ -28,7 +25,56 @@ func (tinder *Tinder) Auth() error {
 	if err != nil {
 		return err
 	}
-	tinder.Headers.Add("X-Auth-Token", user.Token)
+	tinder.Headers.Del("facebook_token")
+	tinder.Headers.Del("facebook_id")
+	tinder.Token = user.Token
 
 	return nil
+}
+
+func (tinder *Tinder) UpdatePreferences(gender string, min_age int, max_age int, max_distance int, bio string) error {
+	var gender_int int
+	switch gender {
+		case "male":
+			gender_int = 0
+		case "female":
+			gender_int = 1
+		default:
+			gender_int = 1
+	}
+
+	JsonStruct := &Profile{
+		Gender: gender_int,
+		Min_Age: min_age,
+		Max_Age: max_age,
+		Max_Distance: max_distance,
+		Bio: bio,
+	}
+	JsonData, err := json.Marshal(JsonStruct)
+	if err != nil {
+		return err
+	}
+
+	JsonReader := bytes.NewReader(JsonData)
+
+	req, err := http.NewRequest("POST", tinder.Host + "/profile", JsonReader)
+	if err != nil {
+		return err
+	}
+	req = tinder.SetRequiredHeaders(req)
+	response, err := tinder.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	return nil
+}
+
+func (tinder *Tinder) SetRequiredHeaders(request *http.Request) *http.Request {
+	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+	request.Header.Set("User-Agent", "Tinder/3.0.4 (iPhone; iOS 7.1; Scale/2.00)")
+	request.Header.Set("X-Auth-Token", tinder.Token)
+
+	return request
 }
